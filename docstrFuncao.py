f0b291e97b2953052e67a36568a509ab7978993a
Megusta80 <gustavo.lopes7804@gmail.com>
2023-03-14 01:24:43 -0300

    Exemplo de docstring em uma função

---
+++ docstrFuncao.py
@@ -0,0 +1,15 @@
+def soma(n1, n2):
+    """Retorna a soma de dois números.
+
+    Parâmetros
+    ----------
+    n1, n2: int ou float
+        Números a serem somados.
+
+    Retorno
+    -------
+    int ou float
+        Resultado da soma de n1 com n2.
+    """
+    s = n1 + n2
+    return s
