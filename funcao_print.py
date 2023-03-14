2c8a0889b45c0b916458edb6f562cce54c366ef8
Megusta80 <gustavo.lopes7804@gmail.com>
2023-03-14 01:22:04 -0300

    Função definida para saída de dados

---
+++ funcao_print.py
@@ -0,0 +1,8 @@
+def soma(n1, n2):
+    print('Início do bloco da função')
+    s = n1 + n2
+    print('A soma é:',s)
+
+print('Fora da função!') 
+resultado1 = soma(3, 5) 
+resultado2 = soma (7, -9)
